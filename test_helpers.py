d651e46131a220044add8a648732db046a51d032
Andrew Phillips <adp1030@gmail.com>
2022-03-30 12:08:41 -0400

    Adding convert_year test cases

--- test_helpers.py
+++ test_helpers.py
@@ -10,7 +10,21 @@ from helpers import (
 )
 
 convert_year_cases = [
-    ()
+    # Check a standard case, with a short list of sequential dates.
+    (pd.Series([["1873", "01"], ["1873", "02"], ["1873", "03"]]),
+     [1873+1/12, 1873+2/12, 1873+3/12]),
+
+    # Check a case when the dates are out of sequential order.
+    (pd.Series([["1873", "04"], ["1773", "01"], ["1973", "03"]]),
+     [1873+4/12, 1773+1/12, 1973+3/12]),
+
+    # Check a case with years with different numbers of digits.
+    (pd.Series([["73", "01"], ["873", "02"], ["1873", "03"]]),
+     [73+1/12, 873+2/12, 1873+3/12]),
+
+    # Check a case with only one date to change.
+    (pd.Series([["2000", "05"]]),
+     [2000+5/12]),
 ]
 
 clean_data_cases = [
@@ -24,10 +38,11 @@ compile_CSVs_cases = [
             'avgHighTemp': range(-8, 51),
             'avgLowTemp': range(-18, 41)
         }, dtype=np.float64)
-    }
+     }
 ]
 
 filter_month_cases = [
+    # Check a standard case, with a short list of sequential date.
     ()
 ]
 
@@ -63,7 +78,7 @@ def test_compile_CSVs(df_dict):
 
     Args:
         df_dict (dictionary): Dictionary containing a singular test dataframe
-    """    
+    """
     assert all(compile_CSVs()[
         'Temperature Experiment'] == df_dict['Temperature Experiment'])
 
